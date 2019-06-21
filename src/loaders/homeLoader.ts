605ea2d230305edb997bbd9721c9bba341f8ed6a
nickgulrajani <nicholasg@gmavt.net>
2019-06-21 08:04:29 -0400

    Sl testing

--- src/loaders/homeLoader.ts
+++ src/loaders/homeLoader.ts
@@ -2,8 +2,14 @@ import * as express from 'express';
 import { MicroframeworkLoader, MicroframeworkSettings } from 'microframework-w3tec';
 
 import { env } from '../env';
-
-export const homeLoader: MicroframeworkLoader = (settings: MicroframeworkSettings | undefined) => {
+/**
+ * EXPRESS TYPESCRIPT BOILERPLATE
+ * ----------------------------------------
+ *
+ * This is a boilerplate for Node.js Application written in TypeScript.
+ * The basic layer of this app is express. For further information visit
+ * the 'README.md' file.
+ */export const homeLoader: MicroframeworkLoader = (settings: MicroframeworkSettings | undefined) => {
     if (settings) {
         const expressApp = settings.getData('express_app');
         expressApp.get(
