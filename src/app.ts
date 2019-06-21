f3e9687517c12534f3778f498ce872e4d6b13662
nickgulrajani <nicholasg@gmavt.net>
2019-06-21 08:03:26 -0400

    SL testing

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,15 @@ import { typeormLoader } from './loaders/typeormLoader';
 import { winstonLoader } from './loaders/winstonLoader';
 
 /**
+ * EXPRESS TYPESCRIPT SL Ingestion 
+ * ----------------------------------------
+ *
+ * This is a boilerplate for Node.js Application written in TypeScript.
+ * The basic layer of this app is express. For further information visit
+ * the 'README.md' file.
+ */
+
+/**
  * EXPRESS TYPESCRIPT BOILERPLATE
  * ----------------------------------------
  *
